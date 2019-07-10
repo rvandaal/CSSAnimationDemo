8d04d854fe5d1d838dd0b096a8395790a469a6e9
Rob van Daal <rvandaal@hotmail.com>
2019-07-10 07:10:27 +0200

    Added other orientations

--- CSSAnimationDemo/src/app/letter-layer/letter-layer.component.ts
+++ CSSAnimationDemo/src/app/letter-layer/letter-layer.component.ts
@@ -7,34 +7,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LetterLayerComponent implements OnInit {
 
+  canvas: HTMLCanvasElement;
+
   constructor() { }
 
   ngOnInit() {
-    const canvas = document.querySelector('canvas');
-    canvas.width = window.innerWidth; // blijkbaar is width en height op 100% zetten via CSS niet genoeg!
-    canvas.height = window.innerHeight;
-    const ctx = canvas.getContext('2d');
+    this.canvas = document.querySelector('canvas');
+    this.canvas.width = window.innerWidth; // blijkbaar is width en height op 100% zetten via CSS niet genoeg!
+    this.canvas.height = window.innerHeight;
+    const ctx = this.canvas.getContext('2d');
 
-    const angle = -Math.PI / 4;
-    const fontsize = 100;
-    this.drawBox(fontsize, 400, 400, angle, ctx);
-    this.drawLetter('WEFZB', fontsize, 400, 400, angle, ctx);
+    const angle = -Math.PI / 1;
+    const fontsize = 60;
+
+    this.drawWord('SNELWANDELEN', 'top', fontsize, ctx);
+    this.drawWord('GOLF', 'bottom', fontsize, ctx);
+    this.drawWord('KICKBOKSEN', 'left', fontsize, ctx);
+    this.drawWord('SPEERWERPEN', 'right', fontsize, ctx);
   }
 
-  drawBox(fontsize: number, x: number, y: number, rotation: number, ctx: CanvasRenderingContext2D) {
-    ctx.strokeStyle = 'black';
-    ctx.save();
-    ctx.translate(x + fontsize / 2, y + fontsize / 2);
-    ctx.rotate(rotation);
-    ctx.strokeRect(-fontsize / 2, -fontsize / 2, fontsize, fontsize);
-    ctx.restore();
+  drawWord(word: string, position: string, fontsize: number, ctx: CanvasRenderingContext2D) {
+    const canvasWidth = window.innerWidth; // this.canvas.width;
+    const canvasHeight = window.innerHeight; // this.canvas.height;
+    const wordWidth = word.length * fontsize;
+
+    for (let i = 0; i < word.length; i++) {
+      const letter = word.charAt(i);
+      if (position === 'top') {
+        this.drawLetter(
+          letter,
+          fontsize,
+          (canvasWidth + wordWidth) / 2 - (i + 1) * fontsize,
+          0.03 * canvasHeight + 50 - fontsize / 2,
+          Math.PI,
+          ctx
+        );
+      } else if (position === 'bottom') {
+        this.drawLetter(
+          letter,
+          fontsize,
+          (canvasWidth - wordWidth) / 2 + i * fontsize,
+          canvasHeight - 0.03 * canvasHeight - 50 - fontsize / 2,
+          0,
+          ctx
+        );
+      } else if (position === 'left') {
+        this.drawLetter(
+          letter,
+          fontsize,
+          0.03 * window.innerHeight + 50 - fontsize / 2,
+          (canvasHeight - wordWidth) / 2 + i * fontsize,
+          Math.PI / 2,
+          ctx
+        );
+      } else if (position === 'right') {
+        this.drawLetter(
+          letter,
+          fontsize,
+          canvasWidth - 0.03 * canvasHeight - 50 - fontsize / 2,
+          (canvasHeight + wordWidth) / 2 - (i + 1) * fontsize,
+          -Math.PI / 2,
+          ctx
+        );
+      }
+    }
   }
 
   drawLetter(letter: string, fontsize: number, x: number, y: number, rotation: number, ctx: CanvasRenderingContext2D) {
-    //ctx.font = 'bold ' + fontsize + 'px Tahoma, sans-serif';
     ctx.font = fontsize + 'px Verdana, sans-serif';
-    //ctx.font = fontsize + 'px Consolas, sans-serif';
-    
     ctx.fillStyle = 'white';
     ctx.textBaseline = 'bottom';
     // Rotate the canvas and draw the text
